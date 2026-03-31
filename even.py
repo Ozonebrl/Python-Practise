6cf02db052c8d9bee4297295854a72d926069984
ozonebrl <ozonebrl7@gmail.com>
2026-03-31 19:39:21 +0545

    pritn even  numbers

---
+++ even.py
@@ -0,0 +1,12 @@
+#Program to print even numbers from 0 to 100
+
+#using for loop to generate even numbers
+for i in range(0,100,2):
+    print(i)
+
+
+# find even numbers from 0 upto a certain number
+num1=int(input("Enter any number: "))
+# using loop to generate even numbers from 0 to num1
+for i in range(0, num1,2):
+    print(i)
