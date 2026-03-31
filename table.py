bcca8ce6aac2fee11acbee44aed062ed0b5ee429
ozonebrl <ozonebrl7@gmail.com>
2026-03-31 19:32:47 +0545

    table of number

---
+++ table.py
@@ -0,0 +1,6 @@
+#this is a program to show table of number in python
+#enter a number
+num=int(input("Enter any number: "))
+#using for loop to generate table
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
