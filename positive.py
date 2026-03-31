09cf51ed0a79ac757b8d02c2c800ac8bca39ccc8
ozonebrl <ozonebrl7@gmail.com>
2026-03-31 19:27:54 +0545

    positive or negative

---
+++ positive.py
@@ -0,0 +1,10 @@
+#This Program helps to find if a number is positive, negative or zero
+# enter any number
+number=int(input("Enter any number: "))
+# condition
+if number>0:
+    print("The number is positive")
+elif number<0:
+    print("The number is negative")
+else:
+    print("The number is Zero")
