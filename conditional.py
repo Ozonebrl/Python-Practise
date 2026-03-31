e92a2eb2bf7aa052b2c9268da0a33846d3030698
ozonebrl <ozonebrl7@gmail.com>
2026-03-31 19:24:13 +0545

    conditional statements

---
+++ conditional.py
@@ -0,0 +1,10 @@
+# This program shows the use of conditional statements in Python
+# Tell user to input a number
+num=int(input("Enter your number: "))
+# check if it is even or odd
+if num%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+ 
